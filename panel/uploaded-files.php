d4cc8d973a3ebee3b7944f37801d46a67248d853
Hossein Fadaei <92361081+ARASHFADAEE@users.noreply.github.com>
2024-12-28 01:17:35 +0330

    add payment method in project

--- panel/uploaded-files.php
+++ panel/uploaded-files.php
@@ -8,20 +8,20 @@ $auth = new Auth();
 $auth->is_login();
 include './config/loader.php';
 
-if ($_SESSION['role']=='user') {
+if ($_SESSION['role'] == 'user') {
     $query_all_file = "SELECT * FROM `files` WHERE user_id=?";
     $result = $conn->prepare($query_all_file);
     $result->bindValue(1, $_SESSION['user_id']);
     $result->execute();
     $files_list = $result->fetchAll(PDO::FETCH_OBJ);
-}elseif($_SESSION['role']=='admin'){
+} elseif ($_SESSION['role'] == 'admin') {
     $query_all_file = "SELECT * FROM `files` ";
     $result = $conn->query($query_all_file);
     $result->execute();
     $files_list = $result->fetchAll(PDO::FETCH_OBJ);
-    
+
 }
-$i=1;
+$i = 1;
 
 //delete file
 if (isset($_GET['delete'])) {
@@ -40,7 +40,6 @@ if (isset($_GET['delete'])) {
         if ($file_data) {
             $file_path = '../uploads/' . $file_data['file_name'];
 
-         
 
             // بررسی دسترسی کاربر به فایل
             if ($file_data['user_id'] === $user_id || $is_admin) {
@@ -72,21 +71,21 @@ if (isset($_GET['delete'])) {
 }
 
 
-$title='uploaded files';
+$title = 'uploaded files';
 
 ?>
 <?php include 'header-main.php'; ?>
 
 <!-- start main content section -->
 <div class="panel mt-6">
-    <?php if ($_SESSION['role']=='admin'):?>
-    <h5 class="text-lg font-semibold dark:text-white-light">Uploaded File in script</h5>
-    <?php elseif ($_SESSION['role']=='user'):?>
+    <?php if ($_SESSION['role'] == 'admin'): ?>
+        <h5 class="text-lg font-semibold dark:text-white-light">Uploaded File in script</h5>
+    <?php elseif ($_SESSION['role'] == 'user'): ?>
         <h5 class="text-lg font-semibold dark:text-white-light">Uploaded File</h5>
 
-    <?php endif;?>
+    <?php endif; ?>
 
-    <?php require_once ('./config/alerts.php')?>
+    <?php require_once('./config/alerts.php') ?>
 
     <div class="dataTable-wrapper dataTable-loading no-footer fixed-columns">
         <div class="dataTable-top"></div>
@@ -102,39 +101,42 @@ $title='uploaded files';
                 </tr>
                 </thead>
                 <tbody>
-                <?php foreach ($files_list as $file){?>
+                <?php foreach ($files_list as $file) { ?>
 
-                <tr>
-                    <td><?= $i++?></td>
-                    <?php
-                    $check=getimagesize($file->file_link);
-                    
-                    if($check):?>
+                    <tr>
+                        <td><?= $i++ ?></td>
+                        <?php
+                        $check = getimagesize($file->file_link);
 
-                    <td><img src="<?= $file->file_link?>" style=" width: 100px" alt=""></td>
-                    
-                    <?php else:?>
-                    
+                        if ($check):?>
 
-                    <td><?= $file->file_name?></td>
-                    <?php endif;?>
-                    <td><?= $file->file_name?></td>
-                    <td>
-                        <a href="./uploaded-files.php?delete=<?= $file->id ?>" style="background: red; padding: 4px; border-radius: 10px; color: #ffff;margin-right: 5px; ">delete</a>
-                        <?php if ($file->type=='indirect'):?>
-                        <a  style="background: blue; padding: 4px; border-radius: 10px; color: #ffff;margin-right: 5px; "  href="<?= $_ENV['SITE_URL']."/?slug=".$file->indirect_slug?>">indirect link</a>
-                         <?php elseif($file->type=='directly'):?>
+                            <td><img src="<?= $file->file_link ?>" style=" width: 100px" alt=""></td>
 
-                        <a  style="background: blue; padding: 4px; border-radius: 10px; color: #ffff;margin-right: 5px; " download href="<?= $file->file_link?>">download</a>
-                         <?php endif;?>
-                    </td>
-                </tr>
-<?php } ?>
+                        <?php else: ?>
+
+
+                            <td><?= $file->file_name ?></td>
+                        <?php endif; ?>
+                        <td><?= $file->file_name ?></td>
+                        <td>
+                            <a href="./uploaded-files.php?delete=<?= $file->id ?>"
+                               style="background: red; padding: 4px; border-radius: 10px; color: #ffff;margin-right: 5px; ">delete</a>
+                            <?php if ($file->type == 'indirect'): ?>
+                                <a style="background: blue; padding: 4px; border-radius: 10px; color: #ffff;margin-right: 5px; "
+                                   href="<?= $_ENV['SITE_URL'] . "/?slug=" . $file->indirect_slug ?>">indirect link</a>
+                            <?php elseif ($file->type == 'directly'): ?>
+
+                                <a style="background: blue; padding: 4px; border-radius: 10px; color: #ffff;margin-right: 5px; "
+                                   download href="<?= $file->file_link ?>">download</a>
+                            <?php endif; ?>
+                        </td>
+                    </tr>
+                <?php } ?>
                 </tbody>
             </table>
         </div>
     </div>
-    
+
 </div>
 
 <?php include 'footer-main.php'; ?>
